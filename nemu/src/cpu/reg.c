fb422ac11c3112597d1e9ace11f888c014629095
tracer-ics2015 <tracer@njuics.org>
2016-10-31 03:22:28 +0800

    >  compile NEMU 141220000 merc Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.36-1+deb8u2 (2016-10-19) i686 GNU/Linux  03:22:28 up 5 days, 16:08,  2 users,  load average: 0.19, 0.08, 0.06 86e23ac7d1449b1ba7c8e6bffe5535ae757e5696

--- nemu/src/cpu/reg.c
+++ nemu/src/cpu/reg.c
@@ -30,7 +30,7 @@ void reg_test() {
 	assert(reg_b(R_DL) == (sample[R_EDX] & 0xff));
 	assert(reg_b(R_DH) == ((sample[R_EDX] >> 8) & 0xff));
 
-	Log("%d %d\n",sample[R_EAX],cpu.eax);
+	Log("%d %d\n",sample[R_ECX],cpu.ecx);
 	assert(sample[R_EAX] == cpu.eax);
 	assert(sample[R_ECX] == cpu.ecx);
 	assert(sample[R_EDX] == cpu.edx);
