38eb85b217a8092172a8b4ea18802e14c653ecb4
tracer-ics2015 <tracer@njuics.org>
2016-10-31 20:50:59 +0800

    >  compile NEMU 141220000 merc Linux debian 3.16.0-4-686-pae #1 SMP Debian 3.16.36-1+deb8u2 (2016-10-19) i686 GNU/Linux  20:50:59 up 6 days,  9:36,  2 users,  load average: 0.04, 0.03, 0.05 1436ff6538bdd3eb8ba2ddb97c9b1642aa36832

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -67,7 +67,6 @@ void cpu_exec(volatile uint32_t n) {
 #ifdef DEBUG
 		print_bin_instr(eip_temp, instr_len);
 		strcat(asm_buf, assembly);
-		Log("%s\n",assembly);
 		Log_write("%s\n", asm_buf);
 		if(n_temp < MAX_INSTR_TO_PRINT) {
 			printf("%s\n", asm_buf);
